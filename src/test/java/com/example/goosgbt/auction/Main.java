84fa0d83a49052e9047788e2022e2e40f489fd44
kjnam <kyujnam@gmail.com>
2018-03-28 21:10:19 +0900

    경매 연결 (~119p)

--- src/test/java/com/example/goosgbt/auction/Main.java
+++ src/test/java/com/example/goosgbt/auction/Main.java
@@ -1,8 +1,22 @@
 package com.example.goosgbt.auction;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+
 import javax.swing.*;
 
 public class Main {
+
+    private static final int ARG_HOSTNAME = 0;
+    private static final int ARG_USERNAME = 1;
+    private static final int ARG_PASSWORD = 2;
+    private static final int ARG_ITEM_ID = 3;
+
+    private static final String AUCTION_RESOURCE = "Auction";
+    private static final String ITEM_ID_AS_LOGIN = "auction-%s";
+    private static final String AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
 
     private MainWindow ui;
 
@@ -12,11 +26,36 @@ public class Main {
 
     public static void main(String... args) throws Exception {
         Main main = new Main();
-    }
-    
-    private void startUserInterface() throws Exception {
-        SwingUtilities.invokeAndWait(() ->
-                ui = new MainWindow()
+        XMPPConnection connection = connectTo(
+                args[ARG_HOSTNAME],
+                args[ARG_USERNAME],
+                args[ARG_PASSWORD]
         );
+        
+        Chat chat = connection.getChatManager().createChat(
+                auctionId(args[ARG_ITEM_ID], connection),
+                (aChat, message) -> {
+                    // TODO something
+                }
+        );
+        
+        chat.sendMessage(new Message());
+    }
+
+    private void startUserInterface() throws Exception {
+        SwingUtilities.invokeAndWait(() -> ui = new MainWindow());
+    }
+
+    private static XMPPConnection connectTo(String hostname, String username, String password) 
+            throws XMPPException {
+        XMPPConnection connection = new XMPPConnection(hostname);
+        connection.connect();
+        connection.login(username, password, AUCTION_RESOURCE);
+        
+        return connection;
+    }
+
+    private static String auctionId(String itemId, XMPPConnection connection) {
+        return String.format(AUCTION_ID_FORMAT, itemId, connection.getServiceName());
     }
 }
