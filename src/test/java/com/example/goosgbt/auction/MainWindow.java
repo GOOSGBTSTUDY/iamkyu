88abce3284318fda0ac0ff404a477b98b0ef811f
kjnam <kyujnam@gmail.com>
2018-03-28 21:06:39 +0900

    스나이퍼 상태 표시 (~117p)

--- src/test/java/com/example/goosgbt/auction/MainWindow.java
+++ src/test/java/com/example/goosgbt/auction/MainWindow.java
@@ -1,6 +1,8 @@
 package com.example.goosgbt.auction;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 
 public class MainWindow extends JFrame {
 
@@ -9,11 +11,21 @@ public class MainWindow extends JFrame {
     public static final String STATUS_JOINING = "Joining";
     public static final String STATUS_LOST = "Lost";
 
+    private final JLabel sniperStatus = createLabel(STATUS_JOINING);
+
     public MainWindow() {
         super("Auction sniper");
         setName(MAIN_WINDOW_NAME);
+        add(sniperStatus);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
         setSize(100, 100);
+    }
+
+    private JLabel createLabel(String initialText) {
+        JLabel result = new JLabel(initialText);
+        result.setName(SNIPER_STATUS_NAME);
+        result.setBorder(new LineBorder(Color.BLACK));
+        return result;
     }
 }
